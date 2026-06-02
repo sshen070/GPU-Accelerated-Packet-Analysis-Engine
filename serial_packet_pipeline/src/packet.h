26dccbbd5359321b321ed809c4565b4c7b00c53e
sshen070 <sujithshen@gmail.com>
2026-06-01 22:35:13 -0700

    Renamed directory from serial_pkt_pipeline & removed gpu files

---
+++ serial_packet_pipeline/src/packet.h
@@ -0,0 +1,25 @@
+#ifndef PACKET_H
+#define PACKET_H
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+// Keep track of pkt details
+struct PacketInfo {
+    uint32_t src_ip;
+    uint32_t dst_ip;
+
+    uint16_t src_port;
+    uint16_t dst_port;
+
+    uint8_t protocol;
+    uint32_t packet_len;
+};
+
+// Reads pcap file & loads pkts into PacketInfo objects
+std::vector<PacketInfo> load_packets(const std::string& filename);
+
+std::string ip_to_string(uint32_t ip);
+
+#endif
